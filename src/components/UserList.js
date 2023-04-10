82d4925f26b37e624444509cd11687a2caec9e90
Fatih <fatihcihan666@gmail.com>
2023-04-10 15:05:12 +0300

    Unnecessary fields cleared, data now comes as props, not state objects

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,51 +1,11 @@
 import React, { Component } from 'react'
 import User from './User';
 
-export class UserList extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            users: [
-                {
-                    id: "85648539",
-                    name: "Fatih",
-                    username: "fatihcihan",
-                    avatar_url: "https://avatars.githubusercontent.com/u/85648539?v=4",
-                    github_url: "https://github.com/fatihcihan",
-                    followers: "8"
-                },
-                {
-                    id: "1",
-                    name: "Okan",
-                    username: "okanbozok",
-                    avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
-                    github_url: "https://github.com/mojombo",
-                    followers: "20"
-                },
-                {
-                    id: "2",
-                    name: "Ekrem",
-                    username: "ekremuysal",
-                    avatar_url: "https://avatars.githubusercontent.com/u/2?v=4",
-                    github_url: "https://github.com/defunkt",
-                    followers: "30"
-                },
-                {
-                    id: "3",
-                    name: "Onur",
-                    username: "onursenelli",
-                    avatar_url: "https://avatars.githubusercontent.com/u/3?v=4",
-                    github_url: "https://github.com/pjhyett",
-                    followers: "40"
-                }
-            ]
-        }
-    }
+export class UserList extends Component {  
     render() {
         return (
             <div>
-                {this.state.users.map(user => (
+                {this.props.users.map(user => (
                     <User user={user} key={user.id} />
                 ))}
             </div>
