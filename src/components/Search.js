a09285e72c8f1c60dc94b7ede286f126d137b408
Fatih <fatihcihan666@gmail.com>
2023-04-10 19:41:58 +0300

    Error messages check

--- src/components/Search.js
+++ src/components/Search.js
@@ -17,8 +17,13 @@ export class Search extends Component {
 
     onSubmit = (e) => {
         e.preventDefault();
-        this.props.searchUsers(this.state.keyword);
-        this.setState({ keyword: "" })
+        if (this.state.keyword === "") {
+            this.props.displayAlert("keyword not found", "danger")
+        } else {
+            this.props.searchUsers(this.state.keyword);
+            this.setState({ keyword: "" })
+        }
+
     }
 
     render() {
