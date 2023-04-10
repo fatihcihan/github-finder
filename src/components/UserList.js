325d309e31dbc52e51676470b560be7f13cd8acd
Fatih <fatihcihan666@gmail.com>
2023-04-10 16:16:42 +0300

    Checked loading from App.js

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react'
 import User from './User';
+import Loading from './Loading';
 
-export class UserList extends Component {  
+export class UserList extends Component {
     render() {
+        if (this.props.loading) {
+            return <Loading />;
+        }
         return (
             <div>
                 {this.props.users.map(user => (
