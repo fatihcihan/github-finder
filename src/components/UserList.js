fe4af7ca91c418397ac1e577ecec17dd45dcfc0c
Fatih <fatihcihan666@gmail.com>
2023-04-10 13:41:40 +0300

    UserList component added (class comp.)

---
+++ src/components/UserList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class UserList extends Component {
+  render() {
+    return (
+      <div>UserList</div>
+    )
+  }
+}
+
+export default UserList
