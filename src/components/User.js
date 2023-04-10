4498af28afce823c0230891f458b94c92e6d2405
Fatih <fatihcihan666@gmail.com>
2023-04-10 14:23:06 +0300

    Unnecessary fields cleared, props from user list displayed before rendering

--- src/components/User.js
+++ src/components/User.js
@@ -2,33 +2,20 @@ import React, { Component } from 'react'
 
 export class User extends Component {
 
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            id: "85648539",
-            name: "Fatih",
-            username: "fatihcihan",
-            avatar_url: "https://avatars.githubusercontent.com/u/85648539?v=4",
-            github_url: "https://github.com/fatihcihan",
-            followers: "8"
-        }
-    }
-
     render() {
+        const { avatar_url, name, followers, github_url } = this.props.user;    //
         return (
-
             <div className="card mb-3">
                 <div className="row g-0">
 
                     <div className="col-md-3">
-                        <img className='img-fluid rounded-start' src={this.state.avatar_url} alt={this.state.name} />
+                        <img className='img-fluid rounded-start avatar' src={avatar_url} alt={name} />
                     </div>
                     <div className="col-md-9">
                         <div className="card-body">
-                            <h3 className='card-title'>{this.state.name}</h3>
-                            <p>Followers: {this.state.followers}</p>
-                            <a target='_blank' href={this.state.github_url} className='btn btn-primary'>Github Profile</a>
+                            <h3 className='card-title'>{name}</h3>
+                            <p> <b> Followers: {followers} </b></p>
+                            <a target='_blank' href={github_url} className='btn btn-primary'>Github Profile</a>
                         </div>
                     </div>
 
