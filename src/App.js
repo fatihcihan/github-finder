4005a417bc25f428bd255b4998bd075a965dc6df
Fatih <fatihcihan666@gmail.com>
2023-04-10 20:32:12 +0300

    Error props sent from app.js to alert component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Alert from './components/Alert';
 import Navbar from './components/Navbar';
 import Search from './components/Search';
 import UserList from './components/UserList';
@@ -33,6 +34,10 @@ export class App extends Component {
       {
         error: { message: message, type: type }
       });
+
+    setTimeout(() => {
+      this.setState({ error: null })
+    }, 3000);
   }
 
   render() {
@@ -44,6 +49,7 @@ export class App extends Component {
           clearResults={this.clearResults}
           showClearButton={this.state.users.length > 0 ? true : false}
           displayAlert={this.displayAlert} />
+        <Alert error={this.state.error} />
         <div className="container mt-3">
           <UserList users={this.state.users} loading={this.state.loading} />
         </div>
